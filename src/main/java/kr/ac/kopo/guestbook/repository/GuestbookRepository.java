7c0346c9a9152945783aa7e0823084f68f01ca74
kjminn <kjminn2001@gmail.com>
2023-05-16 15:31:57 +0900

    PageResultDTO.java 수정(페이지 출력에 필요한 정보)

--- src/main/java/kr/ac/kopo/guestbook/repository/GuestbookRepository.java
+++ src/main/java/kr/ac/kopo/guestbook/repository/GuestbookRepository.java
@@ -4,5 +4,8 @@ import kr.ac.kopo.guestbook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import java.util.stream.IntStream;
+
 public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
+
 }
