852a5ada57429dcd2dd3d998147b5c9bfd19e82d
kjminn <kjminn2001@gmail.com>
2023-05-30 16:29:43 +0900

    수정완료 미완성

--- src/main/java/kr/ac/kopo/guestbook/controller/GuestBookController.java
+++ src/main/java/kr/ac/kopo/guestbook/controller/GuestBookController.java
@@ -8,6 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -39,7 +40,31 @@ public class GuestBookController {
     public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
         log.info("등록처리 후 목록페이지 리다이렉트");
         Long gno = service.register(dto);
-
+        redirectAttributes.addFlashAttribute("msg", gno);
         return "redirect:/guestbook/list";
+    }
+
+    @GetMapping({"/read", "/modify"})
+    public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model){
+        log.info("gno: " + gno);
+        GuestbookDTO dto = service.read(gno);
+        model.addAttribute("dto", dto);
+    }
+
+    @PostMapping("/remove")
+    public  String remove(long gno, RedirectAttributes redirectAttributes){
+        log.info("gno: " + gno);
+        service.remove(gno);
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
+    }
+
+    @PostMapping("/modify")
+    public String modify(GuestbookDTO dto, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, RedirectAttributes redirectAttributes){
+        service.modify(dto);
+
+        redirectAttributes.addFlashAttribute("page", requestDTO.getPage());
+        redirectAttributes.addFlashAttribute("gno", dto.getGno());
+        return "redirect:/guestbook/read";
     }
 }
